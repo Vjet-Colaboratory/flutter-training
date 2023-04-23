5a062f081b3465a0fcfd3e060b5f9be36db95153
Hoa Huu Nguyen <hoaboy89@gmail.com>
2023-04-23 13:31:07 +0700

    Test project with radom status.

---
+++ roll_dice_app/lib/main.dart
@@ -0,0 +1,79 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Quiz App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> _myStatus = [
+    "Status 1",
+    "Status 2",
+    "Status 3",
+    "Status 4",
+    "Status 5"
+  ];
+  int numBer = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      numBer = Random().nextInt(5);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title, style: TextStyle(color: Colors.black12)),
+        centerTitle: true,
+        backgroundColor: Colors.teal[400],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              _myStatus[numBer],
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
